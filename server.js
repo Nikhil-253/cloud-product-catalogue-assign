84ffa94af653b0a231da84a84000e4ee1a48aa6d
Nikhil-253 <154704599+Nikhil-253@users.noreply.github.com>
2025-03-16 17:40:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ const dbConfig = {
     }
 };
 
-// console.log("DB_SERVER:", process.env.DB_HOST);
-// console.log("DB_USER:", process.env.DB_USER);
-// console.log("DB_PASS:", process.env.DB_PASS ? "******" : "Not Set");
-// console.log("DB_NAME:", process.env.DB_NAME);
-// console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_SERVER:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASS:", process.env.DB_PASS ? "******" : "Not Set");
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 
 // Connect to Database
